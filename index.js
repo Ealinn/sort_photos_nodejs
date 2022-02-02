62941deb4078e2669236c408d5a00822733210a5
Sergey Tokarenko <serhii.tokarenko@raiffeisen.ua>
2022-02-03 01:38:49 +0200

    SergeyIndex

--- index.js
+++ index.js
@@ -33,45 +33,120 @@ let copyFiles = function (srcFile, destFile){
 
 // Converts date metadata from UTC time to local 
 let convertDateToGMT = function (dateMsUTC){
+  // console.log("dateMsUTC: ", dateMsUTC);
   let dateObj = {};
-  let date = new Date(dateMsUTC);
+  let date = new Date(dateMsUTC * 1000);
+
+  // console.log("date: ", date);
+
   let options = { year: 'numeric', month: '2-digit', day: '2-digit' };
   let dateGMT = date.toLocaleDateString("uk-UA", options).split('.');
   
+  // console.log("dateGMT: ", dateGMT);
+
   dateObj.year = dateGMT[2];
   dateObj.month = dateGMT[1];
+
+  // console.log("dateObj: ", dateObj);
 
   return dateObj;
 }
 
-let files = getFiles('/Users/ealinn/Samsung_S10_lite'); // array of files in src directory
+// Parse JSON to get photo's creation time
+let notJsonArr = [];
+
+let jsonParse = function (jsonFile){
+  if (fs.existsSync(jsonFile)) {
+    let json = fs.readFileSync(jsonFile);
+    let exif = JSON.parse(json);
+    // console.log(exif);
+
+    return exif;
+  } else {
+    notJsonArr.push(jsonFile);
+  }
+
+
+  // try {
+  //   if (fs.existsSync(jsonFile)) {
+  //     let json = fs.readFileSync(jsonFile);
+  //     let exif = JSON.parse(json);
+  //     // console.log(exif);
+
+  //     return exif;
+  //   }
+  // } catch (err) {
+  //   console.error(err);
+  //   notJsonArr.push(jsonFile);
+  // }
+  // let json = fs.readFileSync(jsonFile);
+  // let exif = JSON.parse(json);
+  // // console.log(exif);
+
+  // return exif;
+}
+
+let files = getFiles('/Users/tok/GooglePhoto-01.02.2022/Takeout'); // array of files in src directory
+
+let i = 0;
 
 let readAndCopyFiles = function (fileURL){
-
+  let createTimeUTC;
   let fileName = fileURL.split('/').reverse()[0];
 
-  // Gets the extension of file
   let fileExt = path.extname(fileName).toLowerCase();
 
+  if ((fileExt == '.jpg') || (fileExt == '.png') || (fileExt == '.mp4') || (fileExt == '.3gp')) {
+    let jsonFile = fileURL + ".json";
+    createTimeUTC = +jsonParse(jsonFile)?.photoTakenTime?.timestamp;
+  }
+
+
+  // console.log(fileName);
+  // console.log(jsonFile);
+
+
+  // console.log("createTimeUTC: ", createTimeUTC);
+
+  // Gets the extension of file
+  
+
   // Exclude files with non specified extensions
-  if ((fileExt != '.jpg') && (fileExt != '.png') && (fileExt != '.mp4')) {
+  if ((fileExt != '.jpg') && (fileExt != '.png') && (fileExt != '.mp4') && (fileExt != '.3gp')) {
     return;
   }
 
-  let metadata = fs.statSync(fileURL);
-  if (!metadata.mtimeMs){
+  // let metadata = fs.statSync(fileURL);
+  // console.log(metadata);
+  // if (!metadata.mtimeMs){
+  //   return;
+  // }
+  if (!createTimeUTC){
     return;
   }
-  
-  let createYear = convertDateToGMT(metadata.mtimeMs).year;
-  let createMonth = convertDateToGMT(metadata.mtimeMs).month;
-  let dirUrlYear = '/Users/ealinn/Sorted_photos/' + createYear; // url for "year directory"
-  let dirUrlMonth = '/Users/ealinn/Sorted_photos/' + createYear + '/' + createMonth; // url for "month directory"
+
+  let createYear = convertDateToGMT(createTimeUTC).year;
+  let createMonth = convertDateToGMT(createTimeUTC).month;
+  // let createYear = convertDateToGMT(metadata.mtimeMs).year;
+  // let createMonth = convertDateToGMT(metadata.mtimeMs).month;
+  let dirUrlYear = '/Users/tok/GooglePhoto-01.02.2022/Sorted/' + createYear; // url for "year directory"
+  let dirUrlMonth = '/Users/tok/GooglePhoto-01.02.2022/Sorted/' + createYear + '/' + createMonth; // url for "month directory"
   let destFile = dirUrlMonth + '/' + fileName; //url for destination file
+
+  console.log(i);
+  console.log("src: ", fileURL);
+  // console.log("createYear: ", createYear);
+  // console.log("createMonth: ", createMonth);
+  console.log("dest: ", destFile);
 
   createDirs(dirUrlYear, dirUrlMonth);
   copyFiles(fileURL, destFile);
 
+  i++;
 }
 
 files.forEach(file => readAndCopyFiles(file));
+// readAndCopyFiles("/Users/tok/GooglePhoto-01.02.2022/Takeout/Google Photos/Photos from 2019/IMG_20191227_222948.jpg");
+let dataNotJsonArr = notJsonArr.toString();
+fs.writeFileSync('/Users/tok/GooglePhoto-01.02.2022/Sorted/notJSON.txt', dataNotJsonArr);
+// console.log(notJsonArr);
